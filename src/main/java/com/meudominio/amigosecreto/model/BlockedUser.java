1497d978bcddf678a6d2a66131c8d6e6c8cba9c8
Otavio2704 <otavioaredes62@gmail.com>
2025-12-06 15:18:26 -0300

    Create BlockedUser.java

---
+++ src/main/java/com/meudominio/amigosecreto/model/BlockedUser.java
@@ -0,0 +1,34 @@
+package com.meudominio.amigosecreto.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "blocked_users", uniqueConstraints = {
+    @UniqueConstraint(columnNames = {"group_id", "blocker_id", "blocked_id"})
+})
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BlockedUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "group_id", nullable = false)
+    private Group group;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "blocker_id", nullable = false)
+    private User blocker;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "blocked_id", nullable = false)
+    private User blocked;
+}

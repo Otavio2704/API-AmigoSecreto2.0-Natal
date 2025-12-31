c4ac3d5017c99accac73d28cdf7c0b56f4a0c15c
Otavio2704 <otavioaredes62@gmail.com>
2025-12-31 15:09:52 -0300

    Refactor GlobalExceptionHandler for improved error handling

--- src/main/java/com/meudominio/amigosecreto/exception/GlobalExceptionHandler.java
+++ src/main/java/com/meudominio/amigosecreto/exception/GlobalExceptionHandler.java
@@ -92,7 +92,7 @@ public class GlobalExceptionHandler {
         log.error("Erro de validação: {}", ex.getMessage());
         
         Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getAllErrors().forEach((error) -> {
+        ex.getBindingResult().getAllErrors().forEach(error -> {
             String fieldName = ((FieldError) error).getField();
             String errorMessage = error.getDefaultMessage();
             errors.put(fieldName, errorMessage);
